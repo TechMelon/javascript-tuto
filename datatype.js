6acf7a99acc473edbc2db51cb350edf752e0cc8a
sneha <snehabhardwaj035@gmail.com>
2024-03-28 07:38:44 +0530

    learn datatypes

---
+++ datatype.js
@@ -0,0 +1,33 @@
+"use strict";
+// treat all JS code as newer version
+
+// alert(3+3) -- we are using node js, not browser
+
+console.log(3+3);
+console.log("Melon");
+let name = "Melon"
+// string data type
+let age = 25
+// number data type
+let isLoggedIn = false
+// boolean data type
+
+
+// number => 2 to the power 53 (range)
+// bigint (used in trading,stocks)
+
+// string => ""
+
+// boolean => true/false
+
+// null => standalone value
+
+// undefined
+
+// symbol => unique
+
+// object
+
+console.log(typeof "Melon");
+console.log(typeof undefined);
+console.log(typeof null);
