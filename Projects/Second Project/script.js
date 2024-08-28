c51c788f37cae51cba402f5a7541798046c27897
sneha <snehabhardwaj035@gmail.com>
2024-08-28 13:06:29 +0530

    new changes

--- Projects/Second Project/script.js
+++ Projects/Second Project/script.js
@@ -13,7 +13,6 @@ form.addEventListener('submit', function(e){
     else if (weight === '' || weight < 0 || isNaN(weight)) {
         results.innerHTML = `Please give a valid weight ${weight}`;
     }
-    // 
     else{
         const bmi = (weight / ((height*height)/10000)).toFixed(2);
         results.innerHTML = `<span>${bmi}</span>`;
